9a081f442c90e05829771c930acc65cbaf7c8723
fraser-brownn <fraserdbrown@yahoo.co.uk>
2023-08-17 21:48:58 +0100

    changed variable names

--- stock_prediciton.sh
+++ stock_prediciton.sh
@@ -1,33 +1,33 @@
 echo -n "Ingest Latest Stock Data? (yes/no):"
-read VAR6
+read VAR1
 
 echo -n "Model Training?(yes/no):"
-read VAR
+read VAR2
 
 kill -9 $(lsof -t -i:8000)
 poetry run uvicorn app.main:app &
 
 export PYTHONPATH=.
 
-if [[ $VAR6 == "yes" ]]
+if [[ $VAR1 == "yes" ]]
 then
   echo "Initiating Data Ingestion"
   bash ingestion/ingestion_pipeline.py
 fi
 
 
-if [[ $VAR == "yes" ]]
+if [[ $VAR2 == "yes" ]]
 then
   echo  "Save Models?: "
-  read VAR2
-
-  echo "Print Evaluation Metrics?: "
   read VAR3
 
-  echo "Create Plots?: "
+  echo "Print Evaluation Metrics?: "
   read VAR4
+
+  echo "Create Plots?: "
+  read VAR5
   echo "Initiating Data Ingestion and Model Training"
-  poetry run python run.py --save $VAR2 --plot $VAR4 --metrics $VAR3
+  poetry run python run.py --save $VAR3 --plot $VAR5 --metrics $VAR4
 
 else
   poetry run python prediction/prediction.py
